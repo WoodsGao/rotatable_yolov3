b3d69046296c4a1caec049ad01e403b50813b70a
woodsgao <woodsgao@outlook.com>
2019-11-14 11:45:01 +0800

    fix

--- train.py
+++ train.py
@@ -153,29 +153,30 @@ def train(lr=1e-3):
 
     train_data = DetectionDataset(
         train_list,
+        cache_size=3000,
         img_size=img_size,
         augments=augments,
     )
 
-    train_loader = DataLoader(
-        train_data,
-        batch_size=batch_size,
-        num_workers=min([os.cpu_count(), batch_size, 16]),
-        shuffle=True,
-        pin_memory=True,
-        collate_fn=train_data.collate_fn)
+    train_loader = DataLoader(train_data,
+                              batch_size=batch_size,
+                              num_workers=min([os.cpu_count(), batch_size,
+                                               16]),
+                              shuffle=True,
+                              pin_memory=True,
+                              collate_fn=train_data.collate_fn)
     val_data = DetectionDataset(
         val_list,
+        cache_size=3000,
         img_size=img_size,
     )
 
-    val_loader = DataLoader(
-        val_data,
-        batch_size=batch_size,
-        num_workers=min([os.cpu_count(), batch_size, 16]),
-        shuffle=True,
-        pin_memory=True,
-        collate_fn=val_data.collate_fn)
+    val_loader = DataLoader(val_data,
+                            batch_size=batch_size,
+                            num_workers=min([os.cpu_count(), batch_size, 16]),
+                            shuffle=True,
+                            pin_memory=True,
+                            collate_fn=val_data.collate_fn)
     # Start training
     model.nc = nc  # attach number of classes to model
     model.arc = opt.arc  # attach yolo architecture
