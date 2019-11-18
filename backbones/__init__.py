c8114508193b0746bd938eb562fd04cf42c06b5a
woodsgao <woodsgao@outlook.com>
2019-11-18 19:45:24 +0800

    add b7

--- backbones/__init__.py
+++ backbones/__init__.py
@@ -2,4 +2,4 @@ from .mini import Mini
 from .resnet import ResNet
 from .xception import Xception
 from .densenet import DenseNet
-from .efficientnet import EfficientNetB2, EfficientNetB4
+from .efficientnet import EfficientNetB2, EfficientNetB4, EfficientNetB7
