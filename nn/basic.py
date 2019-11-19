d8fb6030a25c1f30234fa8b0f3b45eb61dd5bad6
woodsgao <woodsgao@outlook.com>
2019-11-19 14:24:53 +0800

    fix

--- nn/basic.py
+++ nn/basic.py
@@ -15,8 +15,7 @@ class WSConv2d(nn.Conv2d):
         weight_mean = weight_mean.mean(dim=2, keepdim=True)
         weight_mean = weight_mean.mean(dim=3, keepdim=True)
         weight = weight - weight_mean
-        std = weight.view(weight.size(0), -1).std(dim=1).view(-1, 1, 1,
-                                                              1) + 1e-5
+        std = torch.sqrt(torch.var(weight.view(weight.size(0), -1), dim=1) + 1e-12).view(-1, 1, 1, 1) + 1e-5
         weight = weight / std.expand_as(weight)
         return F.conv2d(x, weight, self.bias, self.stride, self.padding,
                         self.dilation, self.groups)
