2b9cfc2892e5a3683d1c4498053c723af9179256
woodsgao <woodsgao@outlook.com>
2019-11-28 19:19:02 +0800

    build all efficientnet

--- backbones/__init__.py
+++ backbones/__init__.py
@@ -2,4 +2,4 @@ from .basic import BasicModel
 from .resnet import ResNet
 from .xception import Xception
 from .densenet import DenseNet
-from .efficientnet import EfficientNetB2, EfficientNetB4, EfficientNetB7
+from .efficientnet import EfficientNet
--- backbones/efficientnet.py
+++ backbones/efficientnet.py
@@ -1,96 +1,66 @@
 import torch.nn as nn
+import math
 from . import BasicModel
 from ..nn import MbBlock, CNS
 
+params_list = [
+    (1, 1, 0.2),
+    (1, 1.1, 0.2),
+    (1.1, 1.2, 0.3),
+    (1.2, 1.4, 0.3),
+    (1.4, 1.8, 0.4),
+    (1.6, 2.2, 0.4),
+    (1.8, 2.6, 0.5),
+    (2, 3.1, 0.5),
+]
 
-class EfficientNetB7(BasicModel):
-    def __init__(self, output_stride=32):
-        super(EfficientNetB7, self).__init__()
-        assert output_stride in [8, 16, 32]
-        if output_stride == 32:
-            block4_stride = 2
-            block5_stride = 2
-        elif output_stride == 16:
-            block4_stride = 2
-            block5_stride = 1
-        elif output_stride == 8:
-            block4_stride = 1
-            block5_stride = 1
+
+class EfficientNet(BasicModel):
+    def __init__(self, model_id=2):
+        super(EfficientNet, self).__init__()
+        params = params_list[model_id]
+        width = [32, 16, 24, 40, 80, 112, 192, 320]
+        width = [math.ceil(w * params[0] / 8) * 8 for w in width]
+        depth = [1, 2, 2, 3, 3, 4, 1]
+        depth = [math.ceil(d * params[1]) for d in depth]
         self.block1 = nn.Sequential(
-            CNS(3, 64, stride=2),
-            MbBlock(64, 32, expand_ratio=1, reps=2, drop_rate=0.5),
+            CNS(3, width[0], stride=2),
+            MbBlock(width[0],
+                    width[1],
+                    expand_ratio=1,
+                    reps=depth[0],
+                    drop_rate=params[2]),
         )
         self.block2 = nn.Sequential(
-            MbBlock(32, 48, stride=2, reps=7, drop_rate=0.5), )
+            MbBlock(width[1],
+                    width[2],
+                    stride=2,
+                    reps=depth[1],
+                    drop_rate=params[2]), )
         self.block3 = nn.Sequential(
-            MbBlock(48, 80, 5, stride=2, reps=7, drop_rate=0.5), )
+            MbBlock(width[2],
+                    width[3],
+                    5,
+                    stride=2,
+                    reps=depth[2],
+                    drop_rate=params[2]), )
         self.block4 = nn.Sequential(
-            MbBlock(80, 160, 5, stride=block4_stride, reps=10, drop_rate=0.5),
-            MbBlock(160, 224, 5, reps=10, drop_rate=0.5),
+            MbBlock(width[3],
+                    width[4],
+                    3,
+                    stride=2,
+                    reps=depth[3],
+                    drop_rate=params[2]),
+            MbBlock(width[4], width[5], 5, reps=depth[4], drop_rate=params[2]),
         )
         self.block5 = nn.Sequential(
-            MbBlock(224, 384, 5, stride=block5_stride, reps=13, drop_rate=0.5),
-            MbBlock(384, 640, 3, reps=4, drop_rate=0.5),
+            MbBlock(width[5],
+                    width[6],
+                    5,
+                    stride=2,
+                    reps=depth[5],
+                    drop_rate=params[2]),
+            MbBlock(width[6], width[7], 3, reps=depth[6], drop_rate=params[2]),
         )
-
-
-class EfficientNetB4(BasicModel):
-    def __init__(self, output_stride=32):
-        super(EfficientNetB4, self).__init__()
-        assert output_stride in [8, 16, 32]
-        if output_stride == 32:
-            block4_stride = 2
-            block5_stride = 2
-        elif output_stride == 16:
-            block4_stride = 2
-            block5_stride = 1
-        elif output_stride == 8:
-            block4_stride = 1
-            block5_stride = 1
-        self.block1 = nn.Sequential(
-            CNS(3, 48, stride=2),
-            MbBlock(48, 24, expand_ratio=1, reps=2, drop_rate=0.4),
-        )
-        self.block2 = nn.Sequential(
-            MbBlock(24, 32, stride=2, reps=4, drop_rate=0.4), )
-        self.block3 = nn.Sequential(
-            MbBlock(32, 56, 5, stride=2, reps=4, drop_rate=0.4), )
-        self.block4 = nn.Sequential(
-            MbBlock(56, 112, stride=block4_stride, reps=6, drop_rate=0.4),
-            MbBlock(112, 160, 5, reps=6, drop_rate=0.4),
-        )
-        self.block5 = nn.Sequential(
-            MbBlock(160, 272, 5, stride=block5_stride, reps=8, drop_rate=0.4),
-            MbBlock(272, 448, 3, reps=2, drop_rate=0.4),
-        )
-
-
-class EfficientNetB2(BasicModel):
-    def __init__(self, output_stride=32):
-        super(EfficientNetB2, self).__init__()
-        assert output_stride in [8, 16, 32]
-        if output_stride == 32:
-            block4_stride = 2
-            block5_stride = 2
-        elif output_stride == 16:
-            block4_stride = 2
-            block5_stride = 1
-        elif output_stride == 8:
-            block4_stride = 1
-            block5_stride = 1
-        self.block1 = nn.Sequential(
-            CNS(3, 32, stride=2),
-            MbBlock(32, 16, expand_ratio=1, reps=2, drop_rate=0.3),
-        )
-        self.block2 = nn.Sequential(
-            MbBlock(16, 24, stride=2, reps=3, drop_rate=0.3), )
-        self.block3 = nn.Sequential(
-            MbBlock(24, 48, 5, stride=2, reps=3, drop_rate=0.3), )
-        self.block4 = nn.Sequential(
-            MbBlock(48, 88, stride=block4_stride, reps=4, drop_rate=0.3),
-            MbBlock(88, 120, 5, reps=4, drop_rate=0.3),
-        )
-        self.block5 = nn.Sequential(
-            MbBlock(120, 208, 5, stride=block5_stride, reps=4, drop_rate=0.3),
-            MbBlock(208, 352, 3, reps=1, drop_rate=0.3),
-        )
+        self.width = width
+        self.depth = depth
