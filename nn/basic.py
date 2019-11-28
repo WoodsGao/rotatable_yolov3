2108a4995e231851098d26710ea5d4e77321b797
woodsgao <woodsgao@outlook.com>
2019-11-28 13:50:53 +0800

    use bn and nn.conv

--- nn/basic.py
+++ nn/basic.py
@@ -11,8 +11,12 @@ class EmptyLayer(nn.Module):
 
 # weight standard conv
 class WSConv2d(nn.Conv2d):
-    def forward(self, x):
-        weight = self.weight
+    def __init__(self, *args, **kwargs):
+        super(WSConv2d, self).__init__(*args, **kwargs)
+        self.fused = False
+
+    def weight_standard(self):
+        weight = self.weight.data
         weight_mean = weight.mean(dim=1, keepdim=True)
         weight_mean = weight_mean.mean(dim=2, keepdim=True)
         weight_mean = weight_mean.mean(dim=3, keepdim=True)
@@ -22,6 +26,21 @@ class WSConv2d(nn.Conv2d):
                         unbiased=False).clamp(min=1e-12)
         std = torch.sqrt(var).view(-1, 1, 1, 1)
         weight = weight / std.expand_as(weight)
+        return weight
+
+    def fuse(self):
+        if not self.fused:
+            self.weight.data = self.weight_standard()
+            self.fused = True
+
+    def forward(self, x):
+        if self.training:
+            weight = self.weight_standard()
+            self.fused = False
+        elif self.fused:
+            weight = self.weight
+        else:
+            weight = self.weight_standard()
         return F.conv2d(x, weight, self.bias, self.stride, self.padding,
                         self.dilation, self.groups)
 
@@ -37,7 +56,7 @@ class CNS(nn.Module):
                  dilation=1,
                  activate=True):
         super(CNS, self).__init__()
-        self.conv = WSConv2d(in_channels,
+        self.conv = nn.Conv2d(in_channels,
                              out_channels,
                              ksize,
                              stride=stride,
@@ -45,12 +64,13 @@ class CNS(nn.Module):
                              groups=groups,
                              dilation=dilation,
                              bias=False)
-        if out_channels % 32 == 0:
-            self.norm = nn.GroupNorm(32, out_channels)
-        elif out_channels % 8 == 0:
-            self.norm = nn.GroupNorm(8, out_channels)
-        else:
-            self.norm = EmptyLayer()
+        # if out_channels % 32 == 0:
+        #     self.norm = nn.GroupNorm(32, out_channels)
+        # elif out_channels % 8 == 0:
+        #     self.norm = nn.GroupNorm(8, out_channels)
+        # else:
+        #     self.norm = EmptyLayer()
+        self.norm = nn.BatchNorm2d(out_channels)
         if activate:
             self.activation = Swish()
         else:
