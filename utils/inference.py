8742ffc3e2e790bf66cbf9f7183d66c4ce03e623
woodsgao <woodsgao@outlook.com>
2020-01-06 10:51:39 +0800

    fix

--- utils/inference.py
+++ utils/inference.py
@@ -6,7 +6,7 @@ from .utils import non_max_suppression, scale_coords
 
 
 @torch.no_grad()
-def inference(model, imgs, img_size=(320, 320), conf_thres=0.3, nms_thres=0.5):
+def inference(model, imgs, img_size=(416, 416), conf_thres=0.3, nms_thres=0.5):
     shapes = [img.shape for img in imgs]
     imgs = [
         cv2.resize(img, img_size)[:, :, ::-1].transpose(2, 0, 1)
@@ -16,7 +16,7 @@ def inference(model, imgs, img_size=(320, 320), conf_thres=0.3, nms_thres=0.5):
     preds = model(imgs)
 
     dets = []
-    for pred, shape, img in zip(preds, shapes, imgs):
+    for pred, shape in zip(preds, shapes):
         # Apply NMS
         det = non_max_suppression(pred, conf_thres, nms_thres)[0]
 
@@ -25,7 +25,7 @@ def inference(model, imgs, img_size=(320, 320), conf_thres=0.3, nms_thres=0.5):
             det = []
         if len(det):
             # Rescale boxes from img_size to im0 size
-            det[:, :4] = scale_coords(img.shape[2:], det[:, :4],
+            det[:, :4] = scale_coords(imgs.shape[2:], det[:, :4],
                                       shape[:2]).round()
         dets.append(det)
     return dets
